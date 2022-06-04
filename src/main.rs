efbfce145a6d73e108f441522a521b5b7bbf0d01
WhiredPlanck <whiredplanck@outlook.com>
2022-06-04 09:35:51 +0800

    main: force require root privilege to directly run cli

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 mod cli;
 
+use std::process;
+
 use anyhow::Result;
 use clap::Parser;
 use cli::SubCommands;
+
+#[inline]
+fn is_root() -> bool {
+    return nix::unistd::geteuid().is_root();
+}
 
 fn main() -> Result<()> {
     let cli = cli::Cli::parse();
 
     match cli.subcommands {
         Some(s) => match s {
-            SubCommands::Cli => {}, 
+            SubCommands::Cli => {
+                if !is_root() {
+                    println!("Please run me as root!");
+                    process::exit(1);
+                }
+            }, 
             SubCommands::Gtk => {}
         },
         None => unreachable!(),

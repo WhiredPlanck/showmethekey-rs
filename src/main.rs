ddb7149ff5a5bdcc01a1f622d4f7c071183eb897
WhiredPlanck <whiredplanck@outlook.com>
2022-06-05 01:51:51 +0800

    main: apply cli::input::run_eventloop to launch cli backend

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod cli;
 
 use std::process;
-
 use anyhow::Result;
 use clap::Parser;
 use cli::SubCommands;
+use input::Libinput;
 
 #[inline]
 fn is_root() -> bool {
@@ -21,6 +21,11 @@ fn main() -> Result<()> {
                     println!("Please run me as root!");
                     process::exit(1);
                 }
+
+                let mut input = Libinput::new_with_udev(cli::input::Interface);
+                input.udev_assign_seat("seat0").unwrap();
+
+                cli::input::run_eventloop(&mut input)?;
             }, 
             SubCommands::Gtk => {}
         },

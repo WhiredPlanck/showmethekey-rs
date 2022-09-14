71922bd9fd618ba3c62f8165a37dba6a4f54d74f
WhiredPlanck <whiredplanck@outlook.com>
2022-09-15 00:38:54 +0800

    cli/main: extract cli logic from subcommand matcher

--- showmethekey-cli/src/main.rs
+++ showmethekey-cli/src/main.rs
@@ -1,9 +1,6 @@
-mod cli;
-
 use std::process;
 use anyhow::Result;
 use clap::Parser;
-use cli::SubCommands;
 use input::Libinput;
 
 #[inline]
@@ -11,26 +8,22 @@ fn is_root() -> bool {
     return nix::unistd::geteuid().is_root();
 }
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+pub struct Cli;
+
 fn main() -> Result<()> {
-    let cli = cli::Cli::parse();
+    Cli::parse();
 
-    match cli.subcommands {
-        Some(s) => match s {
-            SubCommands::Cli => {
-                if !is_root() {
-                    println!("Please run me as root!");
-                    process::exit(1);
-                }
-
-                let mut input = Libinput::new_with_udev(showmethekey_cli::Interface);
-                input.udev_assign_seat("seat0").unwrap();
-
-                showmethekey_cli::run_eventloop(&mut input)?;
-            }, 
-            
-        },
-        None => unreachable!(),
+    if !is_root() {
+        println!("Please run me as root!");
+        process::exit(1);
     }
+
+    let mut input = Libinput::new_with_udev(showmethekey_cli::Interface);
+    input.udev_assign_seat("seat0").unwrap();
+
+    showmethekey_cli::run_eventloop(&mut input)?;
 
     Ok(())
 }

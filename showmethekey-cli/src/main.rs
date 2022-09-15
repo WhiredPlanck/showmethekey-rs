3141847d1d446d64e44bcdaff2fc947d1502466e
WhiredPlanck <whiredplanck@outlook.com>
2022-09-15 20:27:03 +0800

    cli/main: minor refactor
    - change command line display name
    - slightly enhance error handle

--- showmethekey-cli/src/main.rs
+++ showmethekey-cli/src/main.rs
@@ -9,7 +9,7 @@ fn is_root() -> bool {
 }
 
 fn main() -> Result<()> {
-    Command::new("showmethekey-cli")
+    Command::new("showmethekey-cli-rs")
         .version(env!("CARGO_PKG_VERSION"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .get_matches();
@@ -20,9 +20,10 @@ fn main() -> Result<()> {
     }
 
     let mut input = Libinput::new_with_udev(showmethekey_cli::Interface);
-    input.udev_assign_seat("seat0").unwrap();
-
-    showmethekey_cli::run_eventloop(&mut input)?;
+    match input.udev_assign_seat("seat0") {
+        Ok(_) => showmethekey_cli::run_eventloop(&mut input).unwrap(),
+        Err(_) => eprintln!("Failed to set seat."),
+    }
 
     Ok(())
 }

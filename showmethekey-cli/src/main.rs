10423d9f92348d94645036dbe46435f6b3ef773f
WhiredPlanck <whiredplanck@outlook.com>
2023-06-03 23:46:54 +0800

    cli/main: spawn thread to handle input

--- showmethekey-cli/src/main.rs
+++ showmethekey-cli/src/main.rs
@@ -10,6 +10,15 @@ fn is_root() -> bool {
     return nix::unistd::geteuid().is_root();
 }
 
+fn handle_input() {
+    let mut line = String::new();
+    while let Ok(_) = std::io::stdin().read_line(&mut line) {
+        if line == "stop\n" {
+            process::exit(0);
+        }
+    }
+}
+
 fn main() -> Result<()> {
     let args = Command::new("showmethekey-cli-rs")
         .version(env!("CARGO_PKG_VERSION"))
@@ -32,7 +41,17 @@ fn main() -> Result<()> {
 
     let mut input = Libinput::new_with_udev(interface::Interface);
     match input.udev_assign_seat(seat_id) {
-        Ok(_) => interface::run_eventloop(&mut input).unwrap(),
+        Ok(_) => {
+	        // Typically this will be run with pkexec as a subprocess,
+	        // and the parent cannot kill it because it is privileged,
+	        // so we use another thread to see if it gets "stop\n" from stdin,
+	        // it will exit by itself.
+            std::thread::spawn(|| {
+                handle_input();
+            });
+
+            interface::run_eventloop(&mut input).unwrap()
+        }
         Err(_) => eprintln!("Failed to set seat."),
     }
 
